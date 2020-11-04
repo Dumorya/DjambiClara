3e42e7faac6a22a05ae04c816e281d261aa39626
Clara Cassel <clara.tiare@live.fr>
2020-11-04 17:00:51 +0100

    characters created in main file

--- classes/board_cell.py
+++
@@ -1,5 +0,0 @@
-class BoardCell:
-
-    def __init__(self, peons):
-        self.__peons = peons
-
--- main.py
+++ main.py
@@ -2,8 +2,12 @@
 
 # Press Maj+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-from classes.board import Board
+from classes.assassin import Assassin
+from classes.chef import Chef
+from classes.diplomate import Diplomate
 from classes.militant import Militant
+from classes.necromobile import Necromobile
+from classes.reporter import Reporter
 from front.board_template import Application
 import tkinter as tk
 
@@ -18,11 +22,74 @@ nb_columns = 9
 root = tk.Tk()
 app = Application(nb_rows, nb_columns, master=root)
 app.mainloop()
-board1 = Board(nb_rows, nb_columns)
 
-militant1 = Militant(True, "yellow", [1, 7])
-militant2 = Militant(True, "blue", [4, 6])
-militant3 = Militant(True, "green", [1, 6])
-militant4 = Militant(True, "red", [4, 6])
+# MILITANTS #
+militant_green1 = Militant(True, "green", [1, 3])
+militant_green2 = Militant(True, "green", [2, 3])
+militant_green3 = Militant(True, "green", [3, 1])
+militant_green4 = Militant(True, "green", [3, 2])
+
+militant_yellow1 = Militant(True, "yellow", [1, 7])
+militant_yellow2 = Militant(True, "yellow", [2, 7])
+militant_yellow3 = Militant(True, "yellow", [3, 8])
+militant_yellow4 = Militant(True, "yellow", [3, 9])
+
+militant_blue1 = Militant(True, "blue", [7, 8])
+militant_blue2 = Militant(True, "blue", [7, 9])
+militant_blue3 = Militant(True, "blue", [8, 7])
+militant_blue4 = Militant(True, "blue", [9, 7])
+
+militant_red1 = Militant(True, "red", [7, 1])
+militant_red2 = Militant(True, "red", [7, 2])
+militant_red3 = Militant(True, "red", [8, 3])
+militant_red4 = Militant(True, "red", [9, 3])
 
 
+# CHIEFS #
+chief_green = Chef(True, "green", [1, 1])
+
+chief_yellow = Chef(True, "yellow", [1, 9])
+
+chief_blue = Chef(True, "blue", [9, 1])
+
+chief_red = Chef(True, "red", [9, 9])
+
+
+# ASSASSINS #
+assassin_green = Assassin(True, "green", [1, 2])
+
+assassin_yellow = Assassin(True, "yellow", [1, 8])
+
+assassin_blue = Assassin(True, "blue", [9, 8])
+
+assassin_red = Assassin(True, "red", [9, 2])
+
+
+# REPORTERS #
+reporter_green = Reporter(True, "green", [2, 2])
+
+reporter_yellow = Reporter(True, "yellow", [2, 9])
+
+reporter_blue = Reporter(True, "blue", [8, 9])
+
+reporter_red = Reporter(True, "red", [8, 1])
+
+
+# NECROMOBILES #
+necromobile_green = Necromobile(True, "green", [3, 3])
+
+necromobile_yellow = Necromobile(True, "yellow", [3, 7])
+
+necromobile_blue = Necromobile(True, "blue", [7, 3])
+
+necromobile_red = Necromobile(True, "red", [7, 7])
+
+
+# DIPLOMATES #
+diplomate_green = Diplomate(True, "green", [2, 2])
+
+diplomate_yellow = Diplomate(True, "yellow", [2, 8])
+
+diplomate_blue = Diplomate(True, "blue", [8, 2])
+
+diplomate_red = Diplomate(True, "red", [8, 8])
